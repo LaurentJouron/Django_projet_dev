bef011a4ebd4d400b868314b262a13a2fcdd2215
Laurent <jouronlaurent@hotmail.com>
2026-01-19 15:14:52 +0100

    import style modification

--- apps/messages/admin.py
+++ apps/messages/admin.py
@@ -101,3 +101,6 @@ class MessageAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def has_add_permission(self, request):
+        return False
